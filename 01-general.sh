cb9b47b2c9ab63bdf0188426bc2ddb49cfe24c88
Lars Staalhagen <lars@staalhagen.dk>
2026-01-15 21:28:29 +0100

    '20260115-2128'

---
+++ 01-general.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+if [ -z "${BASH}" ] ; then
+  echo "Error: Script must be exercuted with the Bash shell"
+  exit 1
+fi
+
+source ./env
+
+apt-get update
+apt-get -y upgrade
+apt-get -y autoremove
+
+# Miscellaneous other packages
+apt-get -y install build-essential wireshark conntrack conntrackd xterm curl \
+           net-tools ssh gcc-12 bridge-utils jq nmap at gnupg ca-certificates \
+           openvswitch-common openvswitch-switch openvswitch-switch-dpdk openvswitch-doc python3-openvswitch \
+		   doxygen autoconf libtool libssl-dev
+
+# Cleanup apt-get
+apt-get clean
+
+# Stop/disable some irrelevant services
+if [ -s Files/disablelist ] ; then
+  for p in $(cat Files/disablelist) ; do
+    systemctl stop ${p}
+    systemctl mask ${p}
+  done
+fi
+
+# Fix Grub
+sed -i 's/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
+update-grub
+
+# Hack to get a better default font size in Xterm windows
+XRESOURCES=/home/${REALUSER}/.Xresources
+if [ -s $XRESOURCES ]; then
+  grep -q -e "^xterm\*faceName:" $XRESOURCES
+  if [ $? -ne 0 ]; then
+    echo "xterm*faceName: Monospace" >> $XRESOURCES
+    echo "xterm*faceSize: 14" >> $XRESOURCES
+  else
+    sed -i 's/xterm\*faceName:.*/xterm\*faceName: Monospace/g' $XRESOURCES
+    sed -i 's/xterm\*faceSize:.*/xterm\*faceSize: 14/g' $XRESOURCES
+  fi
+else
+  echo "xterm*faceName: Monospace" > $XRESOURCES
+  echo "xterm*faceSize: 14" >> $XRESOURCES
+fi
+chown ${REALUSER}: $XRESOURCES
+sudo -u ${REALUSER} xrdb -merge $XRESOURCES
+
