f6d2dcb5904cd2bb53b321cb58c8747580b2ed22
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2026-01-16 12:01:16 +0100

    '20260116-1201'

--- 34351/02-MininetWiFi-install.sh
+++ 34351/02-MininetWiFi-install.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-# Check for root 
-[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 1
-REALUSER=${SUDO_USER}
-[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit 1
-HOMEDIR=$(eval echo "~$REALUSER")
+if [ -z "${BASH}" ] ; then
+  echo "Error: Script must be exercuted with the Bash shell"
+  exit 1
+fi
 
-# Hack for libssl3 version confusion (NEEDS FIXING FOR FUTURE INSTALLS)
-apt-get install -y --allow-downgrades libssl3=3.0.2-0ubuntu1.12
-apt-get clean
+source ../env
 
-# Create an installation directory and install Mininet-WiFi
+# Create a temporary installation directory and install Mininet-WiFi
 TMPDIR=$(mktemp -d)
 cd ${TMPDIR}
-git clone https://github.com/intrig-unicamp/mininet-wifi || { echo "Git clone failed - exiting" ; exit 1; }
+git clone https://github.com/lstaalhagen/mininet-wifi || { echo "Git clone failed - exiting" ; exit 1; }
 cd mininet-wifi
 util/install.sh -Wlnfv
-cd $HOMEDIR
 rm -rf ${TMPDIR}
 
-sudo -u ${REALUSER} -D ${HOMEDIR} mkdir -p mininet-wifi
-sudo -u ${REALUSER} -D ${HOMEDIR} cp ../MininetWiFi-exercise/skeleton.py mininet-wifi
-
-
-
+mkdir -p ${USERHOME}/mininet-wifi
+cp Files/mininet-wifi/skeleton.py ${USERHOME}/mininet-wifi
+chown -R ${REALUSER}: ${USERHOME}/mininet-wifi
