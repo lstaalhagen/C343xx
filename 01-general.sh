cbb0ee1e98278d2342fae0042de4ef3a47ce4049
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2026-01-21 09:11:50 +0100

    Update 01-general.sh

--- 01-general.sh
+++ 01-general.sh
@@ -3,9 +3,7 @@
 # Check for root
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 0
 REALUSER=${SUDO_USER}
-[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
-
-# source ./env
+[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit 1
 
 apt-get update
 apt-get -y upgrade
