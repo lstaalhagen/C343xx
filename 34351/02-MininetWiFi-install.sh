0aab8560b9b833978e29284aaf466304cecb6442
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2026-01-21 10:06:29 +0100

    Update 02-MininetWiFi-install.sh

--- 34351/02-MininetWiFi-install.sh
+++ 34351/02-MininetWiFi-install.sh
@@ -12,7 +12,7 @@ TMPDIR=$(mktemp -d)
 cd ${TMPDIR}
 git clone https://github.com/lstaalhagen/mininet-wifi || { echo "Git clone failed - exiting" ; exit 1; }
 cd mininet-wifi
-util/install.sh -Wlnv
+util/install.sh -Wlnfv
 rm -rf ${TMPDIR}
 
 cd ${CURRDIR}
