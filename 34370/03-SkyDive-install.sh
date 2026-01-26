99e5f0fecc07a46373754b800f45de6dd86c756b
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2026-01-26 11:07:23 +0100

    Update 03-SkyDive-install.sh

--- 34370/03-SkyDive-install.sh
+++ 34370/03-SkyDive-install.sh
@@ -3,10 +3,10 @@
 # Check for root
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
 
-apt-get update
-apt-get -y install at
+# apt-get update
+# apt-get -y install at
 curl -Lo - https://github.com/skydive-project/skydive-binaries/raw/jenkins-builds/skydive-latest.gz | gzip -d > skydive && chmod +x skydive && sudo mv skydive /usr/local/bin/
 install -m 0755 skydivectl /usr/local/bin/skydivectl
 
 # Cleanup apt-get
-apt-get clean
+# apt-get clean
